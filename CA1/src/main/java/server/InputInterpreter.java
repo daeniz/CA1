d771ad46aafa4fee060c51b1fbfecddbd092a6c0
Danielgron <danielgronbjerg@gmail.com>
2016-09-05 14:06:24 +0200

    Added InputInterpreter

---
+++ CA1/src/main/java/server/InputInterpreter.java
@@ -0,0 +1,14 @@
+/*
+
+    Class used for taking apart the String received from a Client and decifring it.
+
+ */
+package server;
+
+/**
+ *
+ * @author daniel
+ */
+public class InputInterpreter {
+    
+}
