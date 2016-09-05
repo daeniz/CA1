0c2d7b4e87869a9561749467038dedc1d981f9b8
Danielgron <danielgronbjerg@gmail.com>
2016-09-05 13:20:10 +0200

    Started looking at client interpreting

--- CA1/src/main/java/client/Client.java
+++ CA1/src/main/java/client/Client.java
@@ -5,10 +5,38 @@
  */
 package client;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author danie
  */
-public class Client {
-    
+public class Client implements Observer {
+
+    public void processInput(String input) {
+        if (input.isEmpty()) {
+            return;
+        }
+        String[] inputSplit = input.split(":");
+
+        switch (inputSplit[0]) {
+            case "msg":
+                //Do stuff
+                break;
+            case "login":
+                //Do stuff
+                break;
+            case "logout":
+                //Do stuff
+                break;
+        }
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        processInput((String) arg);
+
+    }
 }
