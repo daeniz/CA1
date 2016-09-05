2ed9a63d75c82b8997f8e5f38efba858b9174dba
Danielgron <danielgronbjerg@gmail.com>
2016-09-05 14:08:59 +0200

    Nothing really

--- CA1/src/main/java/server/InputInterpreter.java
+++ CA1/src/main/java/server/InputInterpreter.java
@@ -5,10 +5,37 @@
  */
 package server;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author daniel
  */
-public class InputInterpreter {
-    
+public class InputInterpreter implements Observer{
+    public void processInput(String input) {
+        if (input.isEmpty()) {
+            return;
+        }
+        String[] inputSplit = input.split(":");
+
+        switch (inputSplit[0]) {
+            case "msg":
+                //Do stuff
+                break;
+            case "login":
+                //Do stuff
+                break;
+            case "logout":
+                //Do stuff
+                break;
+        }
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        processInput((String) arg);
+
+    }
 }
